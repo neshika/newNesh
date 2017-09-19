55aa655b6ea2ff07543e084f61274fb30351fad9
neshika <neshika@192.168.8.103>
2017-09-20 00:46:38 +0300

    28. добавка  Всплывающий текст над кнопками
    начало теста кода на расчет энергии/счестья и здоровья

--- test.php
+++ test.php
@@ -12,14 +12,43 @@ require "db.php";
 
 
              echo 'Владелец: ' . $owner=$_SESSION['logged_user']->login;
-             $vitality=12 . '%';
-             $hp=3 . '%';
-             $joy=100 . '%';
+
+if(!isset($_POST['food'])  ){   
+             $_SESSION['vitality']=Rand(1,100);
+             $_SESSION['hp']=Rand(1,100);
+             $_SESSION['joy']=Rand(1,100);
+}
+
+if( isset($_POST['food']) ){        //если кнопка нажата
+$vitality = $_SESSION['vitality'];
+$hp=$_SESSION['hp'];
+$joy=$_SESSION['joy'];
+//title="Энергия +35% Счастье +1%"
+  echo '<br> Энергия' . $vitality;
+  echo '<br> Счастье' . $joy;
+  $vitality=$vitality+35;
+  $joy=$joy+1;
+  echo '<br> Энергия' . $vitality;
+  echo '<br> Счастье' . $joy;
+
+  
+if($vitality>100)
+  $vitality=100;
+if($hp>100)
+  $ph=100;
+if($joy>100)
+  $joy=100;
+
+$_SESSION['vitality']= $vitality;
+$_SESSION['hp']=$hp;
+$_SESSION['joy']=$joy;
+
+$_POST['food']=NULL;
+
+}
+
 
 ?>
-
-                   </colgroup>
-               </table>
 
                <form action="test.php" method="post">
                   <p>Сортировать собак по:
@@ -66,50 +95,53 @@ require "db.php";
 " />добавка</button>
 
 <hr>
+<form method="POST" action="/test.php">
+    <table width="1170" cellpadding="3" cellspacing="0">
+       <colgroup width="390">
+      <tr>
+          <td><input style="float: left;  margin-left: 30px;" id="button" name="food" type="submit" value="есть" title="Энергия +35%
+Счастье +1%" class = "knopka">
+              <input style="float: right;  margin-right: 30px;" id="button" name="knopka" type="submit" value="пить" title="Энергия +5%
+Счастье +1%" class = "knopka">
+              <br>
+              <input style="float: left;  margin-left: 30px;" id="button" name="knopka" type="submit" value="чесать" title="Энергия -3%
+Счастье +10%" class = "knopka">
+              <input style="float: right;  margin-right: 30px;" id="button" name="knopka" type="submit" value="гулять" title="Энергия -17%
+Счастье +15%" class = "knopka">
+              <br>
+              <input style="float: left;  margin-left: 30px;" id="button" name="knopka" type="submit" value="спать" title="Энергия +40%
+Счастье +1%" class = "knopka">
+              <input style="float: right;  margin-right: 30px;" id="button" name="knopka" type="submit" value="растить" class = "knopka">
+        </td>
 
-<table width="1170" cellpadding="3" cellspacing="0">
-   <colgroup width="390">
-  <tr>
-      <td><input style="float: left;  margin-left: 30px;" id="button" name="knopka" type="submit" value="есть" class = "knopka">
-          <input style="float: right;  margin-right: 30px;" id="button" name="knopka" type="submit" value="пить" class = "knopka">
-          <br>
-          <input style="float: left;  margin-left: 30px;" id="button" name="knopka" type="submit" value="чесать" class = "knopka">
-          <input style="float: right;  margin-right: 30px;" id="button" name="knopka" type="submit" value="гулять" class = "knopka">
-          <br>
-          <input style="float: left;  margin-left: 30px;" id="button" name="knopka" type="submit" value="спать" class = "knopka">
-          <input style="float: right;  margin-right: 30px;" id="button" name="knopka" type="submit" value="растить" class = "knopka">
+        <td style="border-width: 10px; text-align: center;"><img src="<?php echo ret_url_from_dna('vip04');?>">
+       
+        </td> 
+        <td>
+          <input id="button" name="knopka" type="submit" value="добавка" title="Энергия +15%
+Счастье +2%" class = "knopka">
+          <input id="button" name="knopka" type="submit" value="спа уход" class = "knopka">
+          <input id="button" name="knopka" type="submit" value="ветеринар" title="Энергия -10%
+Счастье -30%" class = "knopka">
+          <input id="button" name="knopka" type="submit" value="тренировки" title="Энергия -65%
+Счастье +20%" class = "knopka">
+        </td>
+      </tr>
+    </table>
+    <div style="margin-left: 410px""><table>
+            <tr><td>Энергия</td><td><div class="meter"><span style="width: <?php echo$_SESSION['vitality'] . '%';?>"></span></div></td><td><?php echo$_SESSION['vitality'];?></td></tr>
+            <tr><td>Здоровье</td><td><div class="meter"><span style="width: <?php echo$_SESSION['hp'] . '%';?>"></span></div></td><td><?php echo$_SESSION['hp'];?></td></tr>
+            <tr><td>Счастье</td><td> <div class="meter"><span style="width: <?php echo$_SESSION['joy'] . '%';?>"></span></div></td><td><?php echo$_SESSION['joy'];?></td></tr>
 
-        
-    </td>
+          </table>
+    </div>
 
-    <td style="border-width: 10px; text-align: center;"><img src="<?php echo ret_url_from_dna('vip04');?>">
-   
-    </td> 
-    <td>
-      <input id="button" name="knopka" type="submit" value="добавка" class = "knopka">
-      <input id="button" name="knopka" type="submit" value="спа уход" class = "knopka">
-      <input id="button" name="knopka" type="submit" value="ветеринар" class = "knopka">
-      <input id="button" name="knopka" type="submit" value="тренировки" class = "knopka">
-    </td>
-  </tr>
-</table>
-<div style="margin-left: 410px""><table>
-        <tr><td>Энергия</td><td><div class="meter"><span style="width: <?php echo$vitality;?>"></span></div></td><td><?php echo$vitality;?></td></tr>
-        <tr><td>Здоровье</td><td><div class="meter"><span style="width: <?php echo$hp;?>"></span></div></td><td><?php echo$hp;?></td></tr>
-        <tr><td>Счастье</td><td> <div class="meter"><span style="width: <?php echo$joy;?>"></span></div></td><td><?php echo$joy;?></td></tr>
-
-      </table>
-</div>
-
-
+  </colgroup>
+  </table>
+</form>
 <?php  
 
-
-
-
-
 echo '<br>Тестируем! ';
-
 
  require '/libs/down.php';
  ?>
